c3ef7b6a7c4c8f9678aa11df3bacdfa0e4897b5e
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-03-27 22:56:46 +0530

    adding a new to-do is implemented

---
+++ app.js
@@ -0,0 +1,35 @@
+//selectors
+const todoInput = document.querySelector('.todo-input');
+const todoButton = document.querySelector('.todo-button');
+const todoList = document.querySelector('.todo-list');
+
+
+//eventlistners
+todoButton.addEventListener('click',addTodo);
+
+
+//functions
+function addTodo(event){
+    //prevent form from submitting
+    event.preventDefault();
+    const todoDiv= document.createElement('div');
+    todoDiv.classList.add('todo');//adding the class name to the div
+    //create LI
+    const newTodo=document.createElement('li');
+    newTodo.innerText="Hey";
+    newTodo.classList.add('todo-item');
+    todoDiv.appendChild(newTodo);
+    //CHECKED MARK BUTTON
+    const completedButton=document.createElement("button");
+    completedButton.innerHTML='<i class="fas fa-check"></i>';
+    completedButton.classList.add('complete-button');
+    todoDiv.appendChild(completedButton);
+    //Trash BUTTON
+    const trashButton=document.createElement("button");
+    trashButton.innerHTML='<i class="fa-solid fa-trash"></i>';
+    trashButton.classList.add('complete-button');
+    todoDiv.appendChild(trashButton);
+    //APPEND TO List
+    todoList.appendChild(todoDiv);
+
+}
