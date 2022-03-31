af270c5da4ac40d6ae71acbba6d8f347e71eb336
Dinindu Gunathilaka <dinindugunathilaka@gmail.com>
2022-03-31 11:45:27 +0530

    filter implemented

--- app.js
+++ app.js
@@ -2,11 +2,13 @@
 const todoInput = document.querySelector('.todo-input');
 const todoButton = document.querySelector('.todo-button');
 const todoList = document.querySelector('.todo-list');
+const filterOption = document.querySelector(".filter-todo");
 
 
 //eventlistners
 todoButton.addEventListener('click', addTodo);
 todoList.addEventListener('click',deleteCheck);
+filterOption.addEventListener("click", filterTodo);
 
 
 //functions
@@ -55,3 +57,27 @@ function deleteCheck(e){
     }
 
 }
+
+function filterTodo(e) {
+    const todos = todoList.childNodes;
+    todos.forEach(function(todo) {
+      switch (e.target.value) {
+        case "all":
+          todo.style.display = "flex";
+          break;
+        case "completed":
+          if (todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+          break;
+        case "uncompleted":
+          if (!todo.classList.contains("completed")) {
+            todo.style.display = "flex";
+          } else {
+            todo.style.display = "none";
+          }
+      }
+    });
+  }
